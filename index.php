0ac09b50bc176d955b609cfa94d6b89e28230d71
Mark Biek <mark.biek@automattic.com>
2025-02-23 15:45:27 -0500

    Initial add

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Entry point for the Flashcarderator application.
+ * 
+ * @category Application
+ * @package  FCR
+ * @author   Flashcarderator Team <team@flashcarderator.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/flashcarderator
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Initialize application
+use FCR\App;
+
+// Basic test to ensure autoloading works
+echo "Flashcarderator initialized successfully!\n"; 
